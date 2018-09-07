cd43990927e810a9d7bb45509c1eac8dec5c12eb
Ray Olivier <rayolivier@outlook.com>
2018-09-07 15:04:20 -0500

    finished app3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      array: ["ice cream", "cookie", "pie", "cake"],
+      userInput: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(x){
+    this.setState({userInput: x})
+  }
+  
   render() {
+    // let filteredArr = this.state.array.filter((e,i,arr)=>{
+    //     return <h2>{e.includes(this.state.userInput)}</h2>
+    // })
+
+    let listArr = this.state.array.filter((e,i,arr)=>{
+      return e.includes(this.state.userInput)
+    }).map(e=>{
+      console.log(listArr)
+      return <h2>{e}</h2>
+    })
+    
+
+    console.log(listArr)
+
     return (
+
+      
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App 3</h1>
+        <input
+         onChange={(event)=>{this.handleChange(event.target.value)}}
+        ></input>
+        {this.state.userInput}
+        {listArr}
+        {/* {filteredArr} */}
+        {/* <h2 value={this.state.userInput === "" ? listArr : filteredArr}></h2> */}
+
       </div>
     );
   }
